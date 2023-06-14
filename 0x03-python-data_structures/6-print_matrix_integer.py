f201b0785de36cea115e834a5f6b9e1174ab074a
adam <adam@.gmail.com>
2023-06-14 08:47:36 -0700

    dlld

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return (None)
+    for i in range(0, len(matrix)):
+        if matrix[i] is None:
+            continue
+        for j in range(0, len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end = "")
+                    continue
+                print("{:d} ".format(matrix[i][j]), end = "")
+        print("")
