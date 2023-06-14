1303b366e61ee3f3e7647b1eaec43b46cd9c52fa
adam <adam@.gmail.com>
2023-06-14 14:33:12 -0700

    dldl

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        my_tuple = (0, None)
+        return (my_tuple)
+    my_tuple = (len(sentence), sentence[0])
+    return (my_tuple)
