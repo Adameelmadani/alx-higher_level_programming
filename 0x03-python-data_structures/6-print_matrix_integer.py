ecab3c572ef6f82e12592a5caa15857ea8bc2a80
adam <adam@.gmail.com>
2023-06-14 08:52:32 -0700

    dldl

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         if matrix[i] is None:
             continue
         for j in range(0, len(matrix[i])):
-                if j == len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end = "")
-                    continue
-                print("{:d} ".format(matrix[i][j]), end = "")
+            if j == (len(matrix[i]) - 1):
+                print("{:d}".format(matrix[i][j]), end="")
+                continue
+            print("{:d} ".format(matrix[i][j]), end="")
         print("")
