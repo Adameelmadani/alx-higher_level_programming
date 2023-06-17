6625c72da49138525888a71fa9661598053e921d
adam <adam@.gmail.com>
2023-06-17 07:30:24 -0700

    dldl

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if len(a_dictionary) == 0:
+        return None
     i = 0
     for key, value in a_dictionary.items():
         if i == 0:
