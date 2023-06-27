d8e62a0dba1970113ddc038343c3030b4e358e1b
adam <adam@.gmail.com>
2023-06-27 15:00:50 -0700

    dld

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,14 +3,9 @@
 This module contains Square class
 """
 
+
 class Square:
     """
-    A class representing a square.
-
-    This class provides a basic structure for creating and working with square objects.
-
-    Note:
-        This class currently doesn't have any additional methods or attributes defined.
-        It can be extended in the future to add functionality specific to squares.
+    This class doesn't do anything
     """
     pass
