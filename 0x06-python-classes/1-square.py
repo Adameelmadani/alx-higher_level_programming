38e64b38068d96b360d4fda2ba0beedb92d475b6
adam <adam@.gmail.com>
2023-06-27 15:27:06 -0700

    dld

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains Square class
+"""
+
+
+class Square:
+    """
+    This class is the square class
+    """
+    def __init__(self, size=0):
+        """
+        This is the initialisation
+
+        Args:
+             _Square__size (int): size of square
+        """
+        self._Square__size = size
