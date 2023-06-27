93b53490c16a3c19c7c90196175f04c68675c3ac
adam <adam@.gmail.com>
2023-06-26 17:58:24 -0700

    ddl

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    if x < 0:
-        raise Exception("Number is negative")
-    i = j = 0
-    for element in my_list:
-        i = i + 1
-    if x > i:
-        x = i
-    for j in range(0, x):
-        print(my_list[j], end="")
+    j = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
+            j = j + 1
+        except Exception:
+            break
     print("")
-    if i == 0:
-        return (0)
-    return (j + 1)
+    return (j)
