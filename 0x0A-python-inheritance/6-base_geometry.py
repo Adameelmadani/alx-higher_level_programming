0eef6f24c3bd1b7e988495d4a3b676fdea65a6a8
adam <adam@.gmail.com>
2023-07-10 18:42:19 -0700

    dd

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is our module
+"""
+
+
+class BaseGeometry:
+    """
+    This is class
+    """
+    def area(self):
+        """
+        This is p inst meth
+        """
+        raise Exception("area() is not implemented")
