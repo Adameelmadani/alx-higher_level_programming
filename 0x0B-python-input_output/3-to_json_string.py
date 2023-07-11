a0536f6357adf96ef4fedcf0cddd90b3cc95815f
adam <adam@.gmail.com>
2023-07-11 14:16:08 -0700

    dd

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is our module
+"""
+import json
+"""
+json module
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This is our func
+    """
+    return (json.dumps(my_obj))
