9770664bb0578555c83b233aa1a162a7e2476cf5
adam <adam@.gmail.com>
2023-07-10 17:49:18 -0700

    dld

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is our module
+"""
+
+
+class MyList(list):
+    """
+    This is our class
+    """
+    def print_sorted(self):
+        """
+        This is print sorted list
+        """
+        print(sorted(self))
