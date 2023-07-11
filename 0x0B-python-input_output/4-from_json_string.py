496dab21beaaac274fe7f9ea9af772b93882e8b1
adam <adam@.gmail.com>
2023-07-11 14:19:36 -0700

    dld

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is our module
+"""
+import json
+"""
+This is json module
+"""
+
+
+def from_json_string(my_str):
+    """
+    This is our func
+    """
+    return (json.loads(my_str))
