ad1681223ba48708612ec684c0175fc0f273e46a
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-08-17 00:21:20 +0100

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates a table called first_table in the current database in your MySQL server
 CREATE TABLE first_table IF NOT EXISTS(
-  id INTEGER
+  id INTEGER,
   name VARCHAR(256)
   );
