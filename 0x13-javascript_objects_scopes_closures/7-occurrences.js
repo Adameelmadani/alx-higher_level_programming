b3cd7cf466ef7616b3e5564312548f6b2737dd03
Adam <adamelmadani0129@gmail.com>
2023-09-12 15:32:36 -0700

    7.task

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  let i = 0;
+
+  while (list[i]) {
+    if (list[i] === searchElement) { n++; }
+    i++;
+  }
+  return (n);
+};
