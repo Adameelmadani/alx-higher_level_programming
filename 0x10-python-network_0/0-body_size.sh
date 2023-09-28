6ca23e756471adba14330520d07f3a8829bc85b8
Adam <adamelmadani0129@gmail.com>
2023-09-28 23:49:27 +0000

    0.task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Displaying size of http response header in bytes
-line=$(curl -sI "$1" | grep "Content-Length: ")
-line="${line:16}"
-echo "$line"
+echo "$(curl -sI "$1" | grep "Content-Length: " | awk '{print $2}')"
