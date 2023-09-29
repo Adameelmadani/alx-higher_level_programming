6ba1dcbc547ba4e7deaf7fedb0af277cbeecb183
Adam <adamelmadani0129@gmail.com>
2023-09-29 19:38:28 +0000

    5.task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,5 +2,8 @@
 """Fetching url and getting X-request-id"""
 import sys
 import requests
-r = requests.get(sys.argv[1])
-print(r.headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
