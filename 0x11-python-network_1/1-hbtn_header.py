21c04d78029bc14edf73867bd1043d0b3f3b63dc
Adam <adamelmadani0129@gmail.com>
2023-09-29 17:04:04 +0000

    1.task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,5 +2,7 @@
 """Get request so that we print X-request_id in the response header"""
 import sys
 import urllib.request
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
