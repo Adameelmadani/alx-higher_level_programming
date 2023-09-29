2048d37902dc50a646a12bd5ff688337e1c8b4bf
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-09-29 04:12:56 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,13 @@
 """This is our module"""
 def find_peak(list_of_integers):
     """find_pack function"""
-    if list_of_integers.length == 0:
+    if len(list_of_integers) == 0:
         return (None)
-    for i in range(1, list_of_integers.length - 1):
+    if list_of_integers[0] >= list_of_integers[1]:
+        return (list_of_integers[0]);
+    if list_of_integers[len(list_of_integers) - 1] >= list_of_integers[len(list_of_integers) - 2]:
+        return (list_of_integers[len(list_of_integers) - 1]);
+    for i in range(1, len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
             return (list_of_integers[i]);
+    return (None)
