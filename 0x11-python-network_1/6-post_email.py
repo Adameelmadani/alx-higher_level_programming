aaf637ff957dfbf2b2aedd5d2542eaf22b1e86f0
Adam <adamelmadani0129@gmail.com>
2023-09-29 19:57:03 +0000

    6.task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""POST url with email value"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], {'email': sys.argv[2]})
+    print(r)
