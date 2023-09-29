29562c14c98f2bf632adc473a9f01dfb19fc6aca
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-09-29 03:56:40 +0100

    Create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is our module"""
+def find_peak(list_of_integers):
+    if list_of_integers.length == 0:
+        return (None)
+    for i in range(1, list_of_integers.length - 1):
+        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+            return (i);
