f929e8eeff8178dce7ac7fc6bce361e45113a057
Adam <adamelmadani0129@gmail.com>
2023-09-29 23:20:23 +0000

    8.task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fetching and posting variable or json"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ""
+    if len(sys.argv) != 1:
+        q = sys.argv[1]
+    r = requests.post(url, {'q': q})
+    try:
+        a = r.json
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
+    elif len(a) == 0:
+        print("No result")
+    else:
+        print("[{}] {}".format(a.get('id'), a.get('name)))
