04ce53ce4ce08ff4be07619e6073d6baded661f2
Adam <adamelmadani0129@gmail.com>
2023-10-25 00:15:31 +0000

    4.task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const URL = process.argv[2];
-const URL_CHAR = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(URL, function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     let c = 0;
     for (const result of results) {
-      if (result.characters.includes(URL_CHAR)) {
-        c += 1;
+      for (const MY_CHAR of result.characters) {
+        if (MY_CHAR.includes(18)) {
+          c += 1;
+        }
       }
     }
     console.log(c);
